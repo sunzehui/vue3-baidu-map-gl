5a3deee3073f0783f04cffd8d82db9a57639fa09
yue1123 <61490799+yue1123@users.noreply.github.com>
2022-10-10 17:03:24 +0000

    Deploying to gh-pages from  @ 3a58aaf4c242e4d0402112f143a908bd3b26e581 🚀

---
+++ assets/zh_guide_install.md.4f9fada2.js
@@ -0,0 +1,9 @@
+import{_ as a,c as s,o as n,a as e}from"./app.4c479adf.js";const m=JSON.parse('{"title":"\u5B89\u88C5","description":"","frontmatter":{},"headers":[{"level":2,"title":"npm","slug":"npm"},{"level":2,"title":"UMD","slug":"umd"}],"relativePath":"zh/guide/install.md","lastUpdated":1665421346000}'),l={name:"zh/guide/install.md"},p=e(`<h1 id="\u5B89\u88C5" tabindex="-1">\u5B89\u88C5 <a class="header-anchor" href="#\u5B89\u88C5" aria-hidden="true">#</a></h1><h2 id="npm" tabindex="-1">npm <a class="header-anchor" href="#npm" aria-hidden="true">#</a></h2><div class="language-shell"><span class="copy"></span><pre><code><span class="line"><span style="color:#676E95;font-style:italic;"># with npm</span></span>
+<span class="line"><span style="color:#A6ACCD;">npm i vue3-baidu-map-gl</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># or with yarn</span></span>
+<span class="line"><span style="color:#A6ACCD;">yarn add vue3-baidu-map-gl</span></span>
+<span class="line"></span>
+<span class="line"><span style="color:#676E95;font-style:italic;"># or with pnpm</span></span>
+<span class="line"><span style="color:#A6ACCD;">pnpm add vue3-baidu-map-gl</span></span>
+<span class="line"></span></code></pre></div><h2 id="umd" tabindex="-1">UMD <a class="header-anchor" href="#umd" aria-hidden="true">#</a></h2><p>\u53C2\u8003 <a href="/vue3-baidu-map-gl/zh/guide/usage-umd.html">\u4F7F\u7528 UMD</a>\u3002</p>`,5),t=[p];function i(d,r,c,o,h,u){return n(),s("div",null,t)}var f=a(l,[["render",i]]);export{m as __pageData,f as default};
